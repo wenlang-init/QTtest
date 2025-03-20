dbf2179142a13bdfd9b9d9fa919bf1f525c8c75e
wenlang-init <61530615+wenlang-init@users.noreply.github.com>
2025-03-20 12:15:30 +0800

    更新 baseMacroDefine.h

--- nettunnel/public/baseMacroDefine.h
+++ nettunnel/public/baseMacroDefine.h
@@ -1,6 +1,6 @@
 #ifndef BASEMACRODEFINE_H
 #define BASEMACRODEFINE_H
   
-#define TESTSTRING "D:/work/QT/network/nettunnel/sss"
+#define TESTSTRING "D:/work/QT/network/QTtest/nettunnel/sss"
   
 #endif //BASEMACRODEFINE_H
